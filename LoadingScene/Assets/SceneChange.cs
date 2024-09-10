2baedd60956861220abeb1e278d4f2f419938309
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-11 00:02:30 +0900

    Update Loading Script
    loading Script 태스크를 이용하여 구현

---
+++ LoadingScene/Assets/SceneChange.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] Image _loadingImage;
+    [SerializeField] Slider _loadingBar;
+    [SerializeField] Button _button;
+
+    private Coroutine _loadingRoutine;
+    public void ChangeScene(string sceneName)
+    {
+        _button.gameObject.SetActive(false);
+        if (_loadingRoutine != null)
+        {
+            return;
+        }
+        _loadingRoutine = StartCoroutine(LoadingRoutine(sceneName));
+    }
+
+    IEnumerator LoadingRoutine(string sceneName)
+    {
+        AsyncOperation oper = SceneManager.LoadSceneAsync(sceneName);
+
+        oper.allowSceneActivation = false;
+        _loadingImage.gameObject.SetActive(true);
+
+        while (oper.isDone == false)
+        {
+            if (oper.progress < 0.9f)
+            {
+                Debug.Log($"loading = {oper.progress}");
+                _loadingBar.value = oper.progress;
+            }
+            else
+            {
+                Debug.Log("loading Success");
+                break;
+            }
+            yield return null;
+        }
+
+
+        //Fake Loading
+        float time = 0f;
+        while (time < 5f)
+        {
+            time += Time.deltaTime;
+            _loadingBar.value = time / 5f;
+            yield return null;
+        }
+
+        while (Input.anyKeyDown != false)
+        {
+            yield return null;
+        }
+
+        oper.allowSceneActivation = true;
+        _loadingImage.gameObject.SetActive(false);
+    }
+
+
+}
